cbb0f18ba25bbe2d24c5ab107bdd31a76ea0e42a
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-03-20 19:49:15 +0800

    Populated first DGV

--- 6.3_InventoryDashboard.cs
+++ 6.3_InventoryDashboard.cs
@@ -46,21 +46,23 @@ namespace Session6_Kazan_Redo
             dgvCostlyAssets.Columns.Add("Asset Name/Month", "Asset Name/Month");
             using (var context = new Session6Entities())
             {
-                var getDepartments = (from x in context.Departments
-                                      select x.Name).Distinct();
-                var getOrders = (from x in context.Orders
-                                 where x.EmergencyMaintenance.EMStartDate != null && x.EmergencyMaintenance.EMEndDate != null
-                                 orderby x.Date descending
-                                 select x).ToList();
-                var getGroupingByDates = (from x in getOrders
-                                          group x by x.Date.ToString("yyyy/MM") into y
-                                          select y);
 
-                foreach (var distinctDates in getGroupingByDates.Distinct().Take(10))
+                var getDepartments = (from x in context.EmergencyMaintenances
+                                      where x.EMStartDate != null && x.EMEndDate != null
+                                      select x.Asset.DepartmentLocation.Department.Name).Distinct();
+
+                var getDistinctDatesInitial = (from x in context.Orders
+                                               where x.EmergencyMaintenance.EMStartDate != null && x.EmergencyMaintenance.EMEndDate != null
+                                               orderby x.Date descending
+                                               select  x.Date ).ToList();
+                var getDistinctDates = (from x in getDistinctDatesInitial
+                                        select x.ToString("yyyy/MM")).Distinct().Take(10);
+
+                foreach (var distinctDates in getDistinctDates)
                 {
-                    dgvSpending.Columns.Add(distinctDates.Key, distinctDates.Key);
-                    dgvMostUsed.Columns.Add(distinctDates.Key, distinctDates.Key);
-                    dgvSpending.Columns.Add(distinctDates.Key, distinctDates.Key);
+                    dgvSpending.Columns.Add(distinctDates, distinctDates);
+                    dgvMostUsed.Columns.Add(distinctDates, distinctDates);
+                    dgvCostlyAssets.Columns.Add(distinctDates, distinctDates);
 
                 }
 
@@ -68,14 +70,14 @@ namespace Session6_Kazan_Redo
                 {
                     var row = new List<string>();
                     row.Add(departments);
-                    foreach (var distinctDates in getGroupingByDates.Distinct().Take(10))
+                    foreach (var distinctDates in getDistinctDates)
                     {
                         var getList = (from x in context.Orders
                                        where x.EmergencyMaintenance.EMStartDate != null && x.EmergencyMaintenance.EMEndDate != null
                                        where x.EmergencyMaintenance.Asset.DepartmentLocation.Department.Name == departments
                                        select x).ToList();
                         var getSpendings = (from x in getList
-                                            where x.Date.ToString("yyyy/MM") == distinctDates.Key
+                                            where x.Date.ToString("yyyy/MM") == distinctDates
                                             select x.OrderItems.Sum(z => z.Amount * z.UnitPrice)).Sum().Value.ToString("0");
                         row.Add(getSpendings);
 
@@ -83,6 +85,39 @@ namespace Session6_Kazan_Redo
 
                     }
                     dgvSpending.Rows.Add(row.ToArray());
+                }
+
+                foreach (DataGridViewColumn column in dgvSpending.Columns)
+                {
+                    if (dgvSpending.Columns[column.Index].Index == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var dict = new Dictionary<int, decimal>();
+                        foreach (DataGridViewRow row in dgvSpending.Rows)
+                        {
+                            dict.Add(row.Index, Convert.ToDecimal(dgvSpending.Rows[row.Index].Cells[column.Index].Value));
+                        }
+                        var getMax = (from x in dict
+                                      orderby x.Value descending
+                                      where x.Value != 0
+                                      select x.Key).First();
+                        var getMin = (from x in dict
+                                      orderby x.Value
+                                      where x.Value != 0
+                                      select x.Key).First();
+                        if (getMin == getMax)
+                        {
+                            dgvSpending.Rows[getMin].Cells[column.Index].Style.ForeColor = Color.Red;
+                        }
+                        else
+                        {
+                            dgvSpending.Rows[getMin].Cells[column.Index].Style.ForeColor = Color.Red;
+                            dgvSpending.Rows[getMax].Cells[column.Index].Style.ForeColor = Color.Green;
+                        }
+                    }
                 }
 
 
